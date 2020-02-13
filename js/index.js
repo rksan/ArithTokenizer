a2da3974fd5c1f6fc43104cf68c958a9815460fe
rksan <48819681+rksan@users.noreply.github.com>
2020-02-13 14:21:46 +0900

    update index.js
    I forgot the presence of 'jQuery.ready ()'

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 (function ($) {
-    var $document = $(document),
-        $body = $document.find('body'),
-        $form = $body.find('form');
+    $(function _ready() {
+        var $document = $(document),
+            $body = $document.find('body'),
+            $form = $body.find('form');
 
-    $form.find('#go_tokenizer').on('click', function _debug_tokenaizer() {
-        var sequence = $form.find('input[name=input_arith_sequence]').val();
-        var $console = $body.find('#arith_console');
+        $form.find('#go_tokenizer').on('click', function _debug_tokenaizer() {
+            var sequence = $form.find('input[name=input_arith_sequence]').val();
+            var $console = $body.find('#arith_console');
 
-        var token = new _ArithTokenizer(sequence);
+            var token = new _ArithTokenizer(sequence);
 
-        $console.append($('<div />').txt(sequence));
+            $console.append($('<div />').txt(sequence));
 
-        while (token.hasNext() === true) {
-            var sentence = token.next();
+            while (token.hasNext() === true) {
+                var sentence = token.next();
 
-            $console.append($('<div />').txt('"' + sentence.join('') + '"'));
+                $console.append($('<div />').txt('"' + sentence.join('') + '"'));
 
-        }
+            }
+        });
     });
-
 })(window.jQuery);

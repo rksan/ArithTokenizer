2ce80d740c541af79e068e128d4be80c90772411
rksan <48819681+rksan@users.noreply.github.com>
2020-02-13 13:13:37 +0900

    add index.js

--- js/arith-tokenizer.js
+++ js/arith-tokenizer.js
@@ -203,22 +203,3 @@ let _ArithTokenizer = class _ArithTokenizer {
         return current.length === 0 ? undefined : current;
     }
 }
-
-function runTokenizer(sequence) {
-    var token = new _ArithTokenizer(sequence);
-
-    window.console.log('"' + sequence + '"');
-
-    while (token.hasNext() === true) {
-        var sentence = token.next();
-
-        window.console.log('"' + sentence.join('') + '"');
-
-    }
-}
-
-function debug_tokenaizer() {
-    var value = $(document).find('input[name=input_arith_sequence]').val();
-    //var value = '1+2/(3+4)*(5-6)';
-    runTokenizer(value);
-}

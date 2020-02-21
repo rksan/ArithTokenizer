38c943c2778c806881bacc2e6d288465671af8d7
rksan <48819681+rksan@users.noreply.github.com>
2020-02-21 11:01:44 +0900

    update index.js
    Improved the demo so that the contents of the token can be visually grasped

--- js/index.js
+++ js/index.js
@@ -10,14 +10,35 @@
 
             var tokenizer = new _ArithTokenizer(sequence);
 
-            $console.append($('<div />').text(sequence));
+            $console.append($('<div />').append($('<span />').text('sequence:'), $('<span />').text(sequence)));
+
+            var $table = $('<table />');
+            var $tr = $('<tr />');
+            var $th = $('<th />');
+            var $td = $('<td />');
+
+            //header
+            $table.append(
+                $tr.clone().append(
+                    $th.clone().text('token.type'),
+                    $th.clone().text('token.chars')
+                )
+            );
 
             while (tokenizer.hasNext() === true) {
                 var token = tokenizer.next();
 
                 $console.append($('<div />').text('"' + token.toString() + '"'));
 
+                $table.append(
+                    $tr.clone().append(
+                        $td.clone().text(token.type()),
+                        $td.clone().text(token.toString())
+                    )
+                );
             }
+
+            $console.append( $talbe );
         });
     });
 })(window.jQuery);
